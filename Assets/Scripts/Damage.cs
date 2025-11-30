2a149202d1f60aac08761f1ee0b5c566287b64ab
Rin <andris2.faerber-gordon@live.uwe.ac.uk>
2025-11-30 14:02:02 +0000

    Added particles
    ✨magic✨

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -5,4 +5,14 @@ public class Damage : MonoBehaviour
     // script for storing simple attack data
 
     public int damage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health healthObject = collision.gameObject.GetComponent<Health>();
+
+        if (healthObject != null)
+        {
+            healthObject.health -= damage;
+        }
+    }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,5 +4,11 @@ public class Health : MonoBehaviour
 {
     // script for storing simple health data
 
+    public int maxHealth;
     public int health;
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
 }
