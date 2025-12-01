5bb2e225c754ea78066b71c4e17ebcb36085f9e7
Eunie <euan2.gould@live.uwe.ac.uk>
2025-12-01 12:39:15 +0000

    controller support on the upgrade menu
    hard to test without a controller with me, will try to after the merge.

--- Assets/Scripts/UpgradeMenuBehaviour.cs
+++ Assets/Scripts/UpgradeMenuBehaviour.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UpgradeMenuBehaviour : MonoBehaviour
 {
-    public void Unpause()
+    public GameObject defaultOption;
+    [SerializeField] EventSystem eventSystem;
+    
+    public void Unpause(GameObject upgradePrefab)
     {
+        eventSystem.SetSelectedGameObject(defaultOption);
         Time.timeScale = 1.0f;
         GetComponent<Canvas>().enabled = false;
     }
