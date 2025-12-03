d42b8096cda546f3fab65122a507052af64c41c6
Eksteenius <ethan.eksteen00@gmail.com>
2025-12-03 23:57:21 +0000

    health bar test

---
+++ Assets/Scripts/FloatingHealthBar.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingHealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //slider.value;
+    }
+
+    public void UpdateHealthBar() 
+    {
+
+    }
+}
