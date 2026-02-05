d9d59831bade4ffa19c1418ec7418fe057f59b12
Eunie <euan2.gould@live.uwe.ac.uk>
2026-02-05 16:30:12 +0000

    drifting audio
    driftaudio script and prefab functions similar to drift projectiles
    
    main changes are added files and changes to the player prefab and added files

--- Assets/Scripts/DriftAudio.cs
+++ Assets/Scripts/DriftAudio.cs
@@ -7,6 +7,57 @@ public class DriftAudio : MonoBehaviour
     [SerializeField] AudioSource middle;
     [SerializeField] AudioSource end;
 
+
+    [Tooltip("Time spent drifting for debugging")]
+    [SerializeField] private float driftTime = 0;
+
+    [Tooltip("Drift time needed before playing the sound effects")]
+    [SerializeField] private float driftSoundDelay = 0.1f;
+
+
+    private TopDownCarController carController;
+
+    private bool playing = false;
+
+    void Start()
+    {
+        carController = transform.parent.GetComponent<TopDownCarController>();
+    }
+
+
+    void Update()
+    {
+        transform.position = transform.parent.position;
+
+        if (carController.drifting)
+        {
+            driftTime += Time.deltaTime;
+        }
+        else { driftTime = 0; }
+
+        /// Drift Projectiles
+
+        if (carController.drifting && Mathf.Abs(carController.rawSteerInput) > 0.5f && driftTime > driftSoundDelay && driftTime - driftSoundDelay >= 0)
+        {
+            // play audio
+            if (!playing)
+            {
+                beginPlaying();
+                playing = true;
+            }
+        }
+        else
+        {
+
+            if (playing)
+            {
+                playing = false;
+                endPlaying();
+            }
+
+        }
+    }
+
     public void beginPlaying()
     {
         start.Play();
@@ -23,7 +74,11 @@ public class DriftAudio : MonoBehaviour
 
     private void continuePlaying()
     {
-        middle.Play();
-        middle.loop = true;
+        if (playing)
+        {
+            middle.Play();
+            middle.loop = true;
+        }
+
     }
 }
