c23cfb72d6f324f5d12e9d9b8b297c0ec8283cec
Cypress Frost <cypress2.frost@live.uwe.ac.uk>
2026-02-06 23:43:13 +0000

    prototype character customisation
    - added new customisation scene
    - added UI elements to new scene
    - added scripts to change character model, colour and accessories
    - updated truck material/textures
    - added new 3 wheel car player model and textures
    - added new hat accessory
    - changed player prefab in ALPHA with assets to add new options for player model and accessories
    
    my code is kind of rough so if anyone wants to clean it up that would be appreciated :)

---
+++ Assets/UI/Character Customisation/CharacterModelController.cs
@@ -0,0 +1,44 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEditor;
+using UnityEngine;
+
+public class CharacterModelController : MonoBehaviour
+{
+    public GameObject[] models; // Array to hold different character models
+    public GameObject[] truckAccessories; // Array to hold different truck accessories
+    public GameObject[] threeWheeledCarAccessories; // Array to hold different car accessories
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < models.Length; i++)
+        {
+            models[i].SetActive(false); // Deactivates all character models 
+        }
+
+        models[CharacterCustomisation.currentCharacter].SetActive(true); // Activates the currently selected character model
+
+        if (CharacterCustomisation.currentCharacter == 0) // If the current character is the truck
+        {
+            for (int i = 1; i < truckAccessories.Length; i++)
+            {
+                truckAccessories[i].SetActive(false); // Deactivates all truck accessories
+            }
+            truckAccessories[CharacterCustomisation.currentAccessory].SetActive(true); // Activates the currently selected truck accessory
+        }
+        else if (CharacterCustomisation.currentCharacter == 1) // If the current character is the three-wheeled car
+        {
+            for (int i = 1; i < threeWheeledCarAccessories.Length; i++)
+            {
+                threeWheeledCarAccessories[i].SetActive(false); // Deactivates all car accessories
+            }
+            threeWheeledCarAccessories[CharacterCustomisation.currentAccessory].SetActive(true); // Activates the currently selected car accessory
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
