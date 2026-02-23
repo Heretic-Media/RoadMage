15d8698c4baa702455699f6a8bfe9385f49b221f
Rin <andris2.faerber-gordon@live.uwe.ac.uk>
2026-02-23 17:25:13 +0000

    Redone upgrade menu
    Redone ForwardAbility visuals.
    
    Bug: Upgrade menu can select abilities multiple times

--- Assets/Scripts/UpgradeMenuBehaviour.cs
+++ Assets/Scripts/UpgradeMenuBehaviour.cs
@@ -3,42 +3,99 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
 
 public class UpgradeMenuBehaviour : MonoBehaviour
 {
-    [SerializeField] GameObject defaultOption;
+    [SerializeField] GameObject[] optionButtons;
+    int[] options;
     [SerializeField] EventSystem eventSystem;
-    
-    public void Unpause(GameObject upgradePrefab)
+
+    [SerializeField] GameObject[] upgradePrefabs;
+    List<int> availableUpgrades = new List<int>();
+
+    private void Start()
     {
-        eventSystem.SetSelectedGameObject(defaultOption);
+        options = new int[optionButtons.Length];
+        for (int i = 0; i < upgradePrefabs.Length; i++)
+        {
+            availableUpgrades.Add(i);
+        }
+    }
+
+    public void Unpause(int buttonIndex)
+    {
+        Init();
         Time.timeScale = 1.0f;
-        UpgradePlayer(upgradePrefab);
+        UpgradePlayer(options[buttonIndex]);
         GetComponent<Canvas>().enabled = false;
     }
 
-    public void UpgradePlayer(GameObject prefabToUse)
+    public void Pause()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        bool alreadyUpgraded = false;
-
-        for (int i = 0; i < player.transform.childCount; i++)
+        Init();
+        Time.timeScale = 0.0f;
+        for (int i = 0; i < optionButtons.Length; i++)
         {
-            if (prefabToUse.name == player.transform.GetChild(i).name.Replace("(Clone)", ""))
+            options[i] = SelectUpgrades();
+            if (options[i] < 0)
             {
-                alreadyUpgraded = true;
-                break;
+                optionButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = 
+                    "Skip upgrade";
+            }
+            else
+            {
+                optionButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = 
+                    upgradePrefabs[options[i]].name;
             }
         }
-
-        if (!alreadyUpgraded)
-        {
-            GameObject newUpgrade = Instantiate(prefabToUse, player.transform);
-        }
+        GetComponent<Canvas>().enabled = true;
     }
 
-    public void init()
+    public void UpgradePlayer(int prefabIndex)
     {
-        eventSystem.SetSelectedGameObject(defaultOption);
+        if (prefabIndex < 0)
+        {
+            return;
+        }
+
+        GameObject player = FindFirstObjectByType<Player>().gameObject;
+        Instantiate(upgradePrefabs[prefabIndex], player.transform);
+
+        availableUpgrades.Remove(prefabIndex);
+
+        //bool alreadyUpgraded = false;
+
+        //for (int i = 0; i < player.transform.childCount; i++)
+        //{
+        //    if (prefabToUse.name == player.transform.GetChild(i).name.Replace("(Clone)", ""))
+        //    {
+        //        alreadyUpgraded = true;
+        //        break;
+        //    }
+        //}
+
+        //if (!alreadyUpgraded)
+        //{
+        //    GameObject newUpgrade = Instantiate(prefabToUse, player.transform);
+        //}
+    }
+
+    void Init()
+    {
+        eventSystem.SetSelectedGameObject(optionButtons[0]);
+    }
+
+    int SelectUpgrades()
+    {
+        int output = -1;
+        if (availableUpgrades.Count > 0)
+        {
+            int index = Random.Range(0, availableUpgrades.Count);
+            output = availableUpgrades[index];
+        }
+
+        return output;
     }
 }
