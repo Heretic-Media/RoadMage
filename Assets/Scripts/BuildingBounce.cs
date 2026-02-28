fa7e15083292312d78a7b577e4479eab0de7a206
Ethan Eksteen <ethan.eksteen00@gmail.com>
2026-02-28 21:38:02 +0000

    bounce improvement
    fixed code inconsistencies

--- Assets/Scripts/BuildingBounce.cs
+++ Assets/Scripts/BuildingBounce.cs
@@ -24,24 +24,31 @@ public class BuildingBounce : MonoBehaviour
                 Vector3 force = unitCollisionDirection * collisionRB.linearVelocity.magnitude * (1 + bounceMultiplier);
                 Debug.DrawRay(collision.transform.position, force, Color.green, 2f);
                 Debug.Log("BOUNCE: " + force.magnitude.ToString());
-                if (force.magnitude > maxBounceForce) 
-                {
-                    collisionRB.AddForce(unitCollisionDirection * maxBounceForce, ForceMode.VelocityChange);
-                }
-                else if (force.magnitude < minBounceForce) 
-                {
-                    collisionRB.AddForce(unitCollisionDirection * minBounceForce, ForceMode.VelocityChange);
-                }
-                else
-                {
-                    collisionRB.AddForce(force, ForceMode.VelocityChange);
-                }
-            }
 
-            TopDownCarController controller = collision.gameObject.GetComponent<TopDownCarController>();
-            if (controller != null)
-            {
-                controller.disabledTime = disableTime;
+                if (force.magnitude < minBounceForce)
+                {
+                    collisionRB.AddForce(unitCollisionDirection * minBounceForce, ForceMode.Impulse);
+                    collisionRB.AddForce(Vector3.up * minBounceForce * 0.3f, ForceMode.Impulse);
+                }
+                else 
+                {
+                    if (force.magnitude > maxBounceForce)
+                    {
+                        collisionRB.AddForce(unitCollisionDirection * maxBounceForce, ForceMode.Impulse);
+                        collisionRB.AddForce(Vector3.up * maxBounceForce * 0.3f, ForceMode.Impulse);
+                    }
+                    else
+                    {
+                        collisionRB.AddForce(unitCollisionDirection * force.magnitude, ForceMode.Impulse);
+                        collisionRB.AddForce(Vector3.up * force.magnitude * 0.3f, ForceMode.Impulse);
+                    }
+
+                    TopDownCarController controller = collision.gameObject.GetComponent<TopDownCarController>();
+                    if (controller != null)
+                    {
+                        controller.disabledTime = disableTime;
+                    }
+                }
             }
         }
     }
