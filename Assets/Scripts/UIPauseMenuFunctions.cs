bccf0044919c4b1acb0f2bb83cecad3655cba234
Cypress Frost <cypress2.frost@live.uwe.ac.uk>
2026-03-01 10:43:44 +0000

    more functionality
    - all buttons now point to the right place
    - quit now asks for desktop or main menu

--- Assets/Scripts/UIPauseMenuFunctions.cs
+++ Assets/Scripts/UIPauseMenuFunctions.cs
@@ -1,21 +1,70 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class UIPauseMenuFunctions : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject quitConfirmation;
+    public GameObject options;
+    public GameObject codex;
     private int delayFrames = 0;
 
     public void Pause()
     {
         Time.timeScale = 0;
+        options.SetActive(false);
+        codex.SetActive(false);
         pauseMenu.SetActive(true);
     }
 
     public void Resume()
     {
         Time.timeScale = 1;
+        options.SetActive(false);
+        codex.SetActive(false);
         pauseMenu.SetActive(false);
+    }
+
+    public void OpenOptions()
+    {
+        pauseMenu.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void OpenCodex()
+    {
+        pauseMenu.SetActive(false);
+        codex.SetActive(true);
+    }
+
+    public void returnToPauseMenu()
+    {
+        options.SetActive(false);
+        codex.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void AreYouSureYouWantToQuit()
+    {         
+        pauseMenu.SetActive(false);
+        options.SetActive(false);
+        codex.SetActive(false);
+        quitConfirmation.SetActive(true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1;
+        Debug.Log("Returning to main menu...");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1;
+        Debug.Log("Restarting level...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void Quit()
