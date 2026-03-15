5f65a99e0abf75fe95e383e24608c0522c1f9e6c
Cypress Frost <cypress2.frost@live.uwe.ac.uk>
2026-03-15 08:45:22 +0000

    completely innocent chests functional
    - added function for random chest event that can be changed as needed (rn some of the events are placeholder)
    - added chest prefab
    - added chest animations
    - added particle effects to chest

---
+++ Assets/Scripts/Enemies/ChestController.cs
@@ -0,0 +1,88 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ChestController : MonoBehaviour
+{
+    private bool isMimic = false;
+    private int mimicChance = 0;
+    public UnityEvent mimicEvents;
+    public UnityEvent[] chestEvents;
+
+    private bool eventTriggered = false;
+
+    void Awake()
+    {
+        mimicChance = UnityEngine.Random.Range(1, 10);
+        if (mimicChance % 2 == 0)
+        {
+            // even == mimic time
+            isMimic = true;
+            gameObject.name = "MIMIC";
+        }
+        else
+        {
+            isMimic = false;
+        }
+    }
+
+    void MimicTriggered()
+    {
+        if (eventTriggered) return;
+        eventTriggered = true;
+
+        // play animation
+        var animator = gameObject.GetComponent<Animator>();
+        if (animator != null) animator.SetBool("mimicAttacking", true);
+
+        // invoke mimic event
+        mimicEvents.Invoke();
+
+        // destroy game object after attack animation
+        Invoke(nameof(DestroyObject), 4.2f);
+    }
+
+    void ChestOpened()
+    {
+        if (eventTriggered) return;
+        eventTriggered = true;
+
+        // play animation
+        var animator = gameObject.GetComponent<Animator>();
+        if (animator != null) animator.SetBool("chestOpening", true);
+
+        // invoke a random chest event
+        InvokeRandom(chestEvents);
+
+        // destroy object after open animation
+        Invoke(nameof(DestroyObject), 2.5f);
+    }
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("Player detected");
+            if (isMimic) MimicTriggered();
+            else ChestOpened();
+        }
+    }
+
+    private void InvokeRandom(UnityEvent[] events)
+    {
+        if (events == null || events.Length == 0) return;
+
+        int index = UnityEngine.Random.Range(0, events.Length);
+        var evt = events[index];
+        if (evt != null) evt.Invoke();
+    }
+
+    void Update()
+    {
+    }
+}
