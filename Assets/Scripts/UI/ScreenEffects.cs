c8dedbdc88d3ebbffae5d3da8ce381dc9844379d
Cypress Frost <cypress2.frost@live.uwe.ac.uk>
2026-04-10 15:12:52 +0100

    heal effect functional
    - also tweaked damage effect pulse for better triggering for other effects

--- Assets/Scripts/UI/ScreenEffects.cs
+++ Assets/Scripts/UI/ScreenEffects.cs
@@ -1,18 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScreenEffects : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+
     [Header("Effects")]
     [SerializeField] private GameObject healEffect;
+    private bool healPulseActive;
+
     [SerializeField] private GameObject damageEffect;
+    [SerializeField] private int damageThreshold = 1500;
+    [SerializeField] private int crackedThreshold = 750;
     private bool damaged;
     private bool damagePulseActive;
+
     [SerializeField] private GameObject endStateEffect;
+
     [Header("Ability Effects")]
     [SerializeField] private GameObject iceEffect;
+
     [SerializeField] private GameObject fireEffect;
+
     [SerializeField] private GameObject kineticEffect;
 
     private GameObject[] effects;
@@ -50,11 +60,11 @@ public class ScreenEffects : MonoBehaviour
     //damage effect
     public void TriggerDamageEffect()
     {
-        bool isLowHealth = player.GetComponent<Health>().health <= 1500;
+        bool isLowHealth = player.GetComponent<Health>().health <= damageThreshold;
 
         if (isLowHealth)
         {
-            PulseFade();
+            PulseFade(damageEffect, damagePulseActive, 0.13f);
         }
         if (isLowHealth && !damaged)
         {
@@ -71,7 +81,7 @@ public class ScreenEffects : MonoBehaviour
             damagePulseActive = false;
         }
 
-        bool cracked = player.GetComponent<Health>().health <= 750;
+        bool cracked = player.GetComponent<Health>().health <= crackedThreshold;
 
         if (cracked)
         {
@@ -111,23 +121,37 @@ public class ScreenEffects : MonoBehaviour
     }
 
 
-    private void PulseFade()
+    private void PulseFade(GameObject target, bool condition, float maxAlpha)
     {
-        var pulse = damageEffect.transform.GetChild(1).GetComponent<Image>();
+        var pulse = target.transform.GetChild(1).GetComponent<Image>();
 
         var colour = pulse.color;
         float fadeSpeed = 0.1f;
 
-        if (damagePulseActive)
+        if (condition)
         {
             colour.a = Mathf.Clamp01(colour.a - Time.deltaTime * fadeSpeed);
         }
         else
         {
-            colour.a = Mathf.Min(colour.a + Time.deltaTime * fadeSpeed, 0.13f);
+            colour.a = Mathf.Min(colour.a + Time.deltaTime * fadeSpeed, maxAlpha);
         }
 
         pulse.color = colour;
+    }
+
+    // heal effect
+    public void TriggerHealEffect()
+    {
+        healEffect.SetActive(true);
+        healPulseActive = true;
+        Invoke("StopHealEffect", 1f);
+    }
+
+    private void StopHealEffect()
+    {
+        healPulseActive = false;
+        healEffect.SetActive(false);
     }
 
     // Update is called once per frame
@@ -135,5 +159,9 @@ public class ScreenEffects : MonoBehaviour
     {
         TriggerDamageEffect();
 
+        if (healPulseActive)
+        {
+            PulseFade(healEffect, healPulseActive, 0.063f);
+        }
     }
 }
